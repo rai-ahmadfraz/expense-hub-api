d1287c239fbf50f2c33f749863a426815e381a02
raiahmadfraz <rai@repairdesk.co>
2026-01-04 00:05:13 +0500

    db and port change

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { FriendModule } from './friend/friend.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'RedEye1384',
+      password: 'RedEye1384@',
       database: 'expense',
 
       // ===== PRODUCTION (Railway) =====
